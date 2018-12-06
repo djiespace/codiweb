a48e97bb09f0097a34836244707b9abbe88d6c6c
Marketadjie <marketadjie@gmail.com>
2018-12-06 09:44:35 +0700

    commit next

--- header.php
+++ header.php
@@ -3,6 +3,13 @@
 ?>
 <html>
 <head>
-    
+    <title>Dashboard | pendaftaran santri baru</title>
+    <link rel="stylesheet" type="text/css" href="Asset/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="Asset/font-awesome/css/font-awesome.css"/>
+	<link rel="stylesheet" type="text/css" href="Asset/css/main.css"/>
+	<!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<script type="text/javascript" src="asset/js/jquery.js"></script>
+	<script type="text/javascript" src="asset/js/bootstrap.min.js"></script>
 </head>
 <body>
